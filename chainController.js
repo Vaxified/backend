060b1e2389c3250034e080f80c658ebb4cf6ff65
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2021-01-16 22:19:28 -0800

    added chain validity certifier

--- chainController.js
+++ chainController.js
@@ -1,43 +1,71 @@
 // contactController.js
 // Import contact model
-const SHA256 = require('crypto-js/sha256');
-const express = require('express');
+const SHA256 = require("crypto-js/sha256");
+const express = require("express");
 Blockchain = require("./blockModel");
 // Handle index actions
+
+let calculateHash = (index, previousHash, date, name) => {
+  return SHA256(index + previousHash + name + JSON.stringify(date)).toString();
+};
+
 exports.index = function (req, res) {
+  let chainValidity;
   Blockchain.get(function (err, blocks) {
+    for (i = 1; i < blocks.length; i++) {
+      const currentBlock = blocks[i];
+      const previousBlock = blocks[i - 1];
+      if (
+        currentBlock.hash !==
+        calculateHash(
+          currentBlock.index,
+          currentBlock.previousHash,
+          currentBlock.date,
+          currentBlock.name
+        )
+      ) {
+        chainValidity = false;
+      } else if (currentBlock.previousHash !== previousBlock.hash) {
+        chainValidity = false;
+      }
+    }
+    chainValidity = (chainValidity = false) ? false : true;
+
     if (err) {
       res.json({
         status: "error",
         message: err,
       });
+    } else if ((chainValidity == false)) {
+      res.json({
+        message: "The chain has been corrupted",
+      });
+    } else {
+      res.json({
+        data: blocks,
+      });
     }
-    res.json({
-      data: blocks,
-    });
   });
 };
 
-let calculateHash = (index,previousHash,date, name) =>{
-  return SHA256(index + previousHash + name + JSON.stringify(date)).toString();
-}
-
 // Handle create contact actions
-exports.new = async function (req, res)  {
+exports.new = async function (req, res) {
   chain = await Blockchain.find({});
   chainLenght = Object.keys(chain).length;
-  previousHash = (chain[chainLenght-1] != null? chain[chainLenght-1].hash: "")
-
-  
-
-  
+  previousHash =
+    chain[chainLenght - 1] != null ? chain[chainLenght - 1].hash : "";
 
   var block = new Blockchain();
   block.name = req.body.name;
   block.index = chainLenght;
-  block.date = req.body.date
+  block.date = req.body.date;
   block.previousHash = previousHash;
-  block.hash = calculateHash(block.index,block.previousHash,block.date, block.name)
+  block.hash = calculateHash(
+    block.index,
+    block.previousHash,
+    block.date,
+    block.name
+  );
   // save the contact and check for errors
   block.save(function (err) {
     res.json({
@@ -46,7 +74,6 @@ exports.new = async function (req, res)  {
   });
 };
 
-exports.delete = function(req, res){
-  Blockchain.collection.remove({})
-}
-
+exports.delete = function (req, res) {
+  Blockchain.collection.remove({});
+};
