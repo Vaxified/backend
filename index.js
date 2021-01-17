a09bdfc4eecb01b7e3d5bac4fb64a50e880eea36
Henrymarks1 <58492005+Henrymarks1@users.noreply.github.com>
2021-01-16 23:20:03 -0800

    connected to atlus

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let express = require("express");
 let bodyParser = require("body-parser");
 let mongoose = require("mongoose");
 let app = express();
+const uri = "mongodb+srv://henry123:henry123@cluster0.tainw.mongodb.net/vaxified?retryWrites=true&w=majority";
 
 let apiRoutes = require("./api-routes");
 app.use(
@@ -11,7 +12,7 @@ app.use(
 );
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/vaxified", { useNewUrlParser: true });
+mongoose.connect(uri, { useNewUrlParser: true });
 var db = mongoose.connection;
 
 if (!db) console.log("Error connecting db");
